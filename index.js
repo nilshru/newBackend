c4868e19ecca8297864818708989f73d2191a674
nilshru <nilesh30032004@gmail.com>
2025-06-19 12:19:27 +0530

    fix: use process.env.PORT for Render deploy

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 4000 || process.env.PORT
+const port = process.env.PORT || 4000;
+
 
 app.get('/', (req, res) => {
   res.send('This is home page by Nilesh')
